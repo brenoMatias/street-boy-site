89a41928fd634e2c452fa017ec946f0e8ff0f86f
brenoMatias <matiasbreno19@gmail.com>
2022-07-24 19:27:00 -0300

    updates

---
+++ src/PAGES/StreetBoy/streetBoy.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './boyCss.css';
+import HeaderBoy from '../../COMPONENTS/HeaderBoy/headerBoy';
+
+function StreetBoy() {
+    return (
+        <div>
+            <HeaderBoy />
+        </div>
+
+    )
+}
+
+export default StreetBoy;
